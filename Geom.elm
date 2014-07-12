9806085b0c165e54367653fcab2d4628c56f076b
Omar Rizwan <omar.rizwan@gmail.com>
2014-07-11 19:47:30 -0700

    draws touches (in place -- collage coordinate conversion logic!) now, also beginning some work on distinguishing two-finger vs one-finger touches (fingers vs pencils)

--- Geom.elm
+++ Geom.elm
@@ -1,10 +1,13 @@
 import Window
 import Touch as T
 
-fingers : Signal [T.Touch]
+type Finger = T.Touch
+type Pencil = T.Touch
+
+fingers : Signal [Finger]
 fingers = T.touches
 
-pencils : Signal [T.Touch]
+pencils : Signal [Pencil]
 pencils = T.touches
 
 type DrawState = { lines : [Line]
@@ -49,4 +52,42 @@ display { lines, arcs, toolCenter, toolAngle, toolLength, drawing } =
         layers [ collage 800 600 <| map (traced defaultLine . uncurry segment) lines
                , displayTool toolCenter toolAngle toolLength  ]
 
-main = display <~ (foldp draw initialDrawState <| lift2 (,) fingers pencils)
+displayS : Signal Element
+displayS = display <~ (foldp draw initialDrawState <| lift2 (,) fingers pencils)
+
+recenter : Int -> Int -> T.Touch -> T.Touch
+recenter ww wh t = { t | x <- t.x - (ww `div` 2),
+                         y <- -t.y + (wh `div` 2) }
+
+distinguish : [T.Touch] -> ([Finger], [Pencil])
+distinguish ts = let examineTouch t (fs, ps) =
+                       let distances = map (\t2 -> ((t2.x - t.x)^2 + (t2.y - t.y)^2, t2)) ts
+                           nearer (d1, t1) (d2, t2) = if d1 < d2
+                                                        then (d1, t1)
+                                                        else (d2, t2)
+                           (nearestDist, nearestTouch) = foldr1 nearer distances
+                       in if nearestDist < 40
+                            then (nearestTouch :: fs, ps)
+                            else (fs, t :: ps)
+                 in foldr examineTouch ([], []) ts
+
+touchesR : Signal [T.Touch]
+touchesR = let recenterAll ww wh ts = map (recenter ww wh) ts
+           in recenterAll <~ Window.width ~ Window.height ~ T.touches
+
+touchSymbol : T.Touch -> Form
+touchSymbol {x, y} = circle 10
+                   |> outlined defaultLine
+                   |> move (toFloat x, toFloat y)
+
+touchesView : Signal Element
+touchesView = let viewTouches ww wh ts = collage ww wh <| map touchSymbol ts
+              in viewTouches <~ Window.width 
+                              ~ Window.height
+                              ~ touchesR
+
+main = (\x y -> layers [x, y]) <~ touchesView ~ displayS
+
+-- TODO put dots for fingers and/or pencils
+-- start working on rotation, drawing, etc
+-- figure out how to normalize coordinates / get window dims into collage
