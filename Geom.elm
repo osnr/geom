080ed868fe63c16a0b64fda9d9cd9474eb43f3a1
Omar Rizwan <omar.rizwan@gmail.com>
2014-07-12 14:53:09 -0700

    put blue background on usable canvas. let us draw lines (maybe I should identify touches by id.. well, whatever).

--- Geom.elm
+++ Geom.elm
@@ -25,7 +25,15 @@ initialDrawState = { lines = []
                    , drawing = NotDrawing }
 
 draw : ([Finger], [Pencil]) -> DrawState -> DrawState
-draw (fs, ps) ds = ds
+draw (fs, ps) ds =
+  case (ps, ds.drawing) of
+    ([], NotDrawing) -> ds
+    ([], DrawingLine l) -> { ds | drawing <- NotDrawing,
+                                  lines <- l :: ds.lines }
+    ([], DrawingArc a) -> { ds | drawing <- NotDrawing,
+                                 arcs <- a :: ds.arcs }
+    (p::_, NotDrawing) -> { ds | drawing <- DrawingLine ((toFloat p.x, toFloat p.y), (toFloat p.x, toFloat p.y)) }
+    (p::_, DrawingLine (pt1, _)) -> { ds | drawing <- DrawingLine (pt1, (toFloat p.x, toFloat p.y)) }
 
 toolTop : Float -> Form
 toolTop len = rect (len*5) 20 |> outlined defaultLine
@@ -41,37 +49,44 @@ displayTool c angle len = group [ toolTop len |> moveY 11
                         |> (\x -> [x])
                         |> collage 800 600
 
-display : DrawState -> Element
-display { lines, arcs, toolCenter, toolAngle, toolLength, drawing } =
-        layers [ collage 400 300 <| map (traced defaultLine . uncurry segment) lines
+drawingLines : Drawing -> [Line]
+drawingLines drawing = case drawing of
+                         NotDrawing -> []
+                         DrawingLine l -> [l]
+                         DrawingArc _ -> []
+
+display : Int -> Int -> DrawState -> Element
+display ww wh { lines, arcs, toolCenter, toolAngle, toolLength, drawing } =
+        layers [ collage ww wh <| map (traced defaultLine . uncurry segment) <| drawingLines drawing ++ lines
                , displayTool toolCenter toolAngle toolLength  ]
 
 displayS : Signal Element
-displayS = display <~ (foldp draw initialDrawState <| fingersPencils)
+displayS = display <~ Window.width ~ Window.height ~ (foldp draw initialDrawState <| fingersPencils)
 
 recenter : Int -> Int -> T.Touch -> T.Touch
 recenter ww wh t = { t | x <- t.x - (ww `div` 2),
                          y <- -t.y + (wh `div` 2) }
 
 distinguish : [T.Touch] -> ([Finger], [Pencil])
-distinguish ts = let examineTouch t (fs, ps, used) =
-                       let findDistance t1 t2 = (sqrt <| (t2.x - t.x)^2 + (t2.y - t.y)^2, t2)
-                           distances = map (findDistance t) <| filter (\t2 -> t /= t2) ts
-                           nearer (d1, t1) (d2, t2) = if d1 < d2
-                                                        then (d1, t1)
-                                                        else (d2, t2)
-                       in if | isEmpty distances -> (fs, t :: ps, used)
-                             | any (\t2 -> t == t2) used -> (fs, ps, used)
-                             | otherwise ->
-                               let (nearestDist, nearestTouch) = foldr1 nearer distances
-                               in if nearestDist < 80
-                                    then ({ t | x <- (nearestTouch.x + t.x) `div` 2,
-                                                y <- (nearestTouch.y + t.y) `div` 2  } :: fs,
-                                          ps,
-                                          t :: nearestTouch :: used)
-                                    else (fs, t :: ps, used)
-                     (fs, ps, _) = foldr examineTouch ([], [], []) ts
-                 in (fs, ps)
+distinguish ts =
+  let examineTouch t (fs, ps, used) =
+        let findDistance t1 t2 = (sqrt <| (t2.x - t.x)^2 + (t2.y - t.y)^2, t2)
+            distances = map (findDistance t) <| filter (\t2 -> t /= t2) ts
+            nearer (d1, t1) (d2, t2) = if d1 < d2
+                                         then (d1, t1)
+                                         else (d2, t2)
+        in if | isEmpty distances -> (fs, t :: ps, used)
+              | any (\t2 -> t == t2) used -> (fs, ps, used)
+              | otherwise ->
+                let (nearestDist, nearestTouch) = foldr1 nearer distances
+                in if nearestDist < 80
+                     then ({ t | x <- (nearestTouch.x + t.x) `div` 2,
+                                 y <- (nearestTouch.y + t.y) `div` 2  } :: fs,
+                           ps,
+                           t :: nearestTouch :: used)
+                     else (fs, t :: ps, used)
+      (fs, ps, _) = foldr examineTouch ([], [], []) ts
+  in (fs, ps)
 
 touchesR : Signal [T.Touch]
 touchesR = let recenterAll ww wh ts = map (recenter ww wh) ts
@@ -91,7 +106,8 @@ pencilSymbol {x, y} = rect 50 50
                     |> move (toFloat x, toFloat y)
 
 touchesView : Signal Element
-touchesView = let viewTouches ww wh (fs, ps) = collage ww wh
+touchesView = let viewTouches ww wh (fs, ps) = color blue
+                                               <| collage ww wh
                                                <| map fingerSymbol fs ++ map pencilSymbol ps
               in viewTouches <~ Window.width 
                               ~ Window.height
