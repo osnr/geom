12c200cbc466ac946478a7ea910f444b7aca174c
Omar Rizwan <omar.rizwan@gmail.com>
2014-07-29 14:05:31 -0700

    drawing lines works -- but they don't get permanently saved yet. arcs are like halfway there

--- Geom.elm
+++ Geom.elm
@@ -8,46 +8,50 @@ type NTouch = { x:Float, y:Float, id:Int, x0:Float, y0:Float, t0:Time }
 
 type DrawState = { lines : [Line]
                  , arcs : [Arc]
-                 , toolCenter : Point
+                 , toolStart : Point
                  , toolAngle : Angle
                  , toolLength : Length
-                 , gesture : Gesture }
+                 , gesture : Gesture
+                 , drawing : Drawing }
 
 type Point = (Float, Float)
 type Angle = Float
 type Length = Float
 
 type Line = (Point, Point)
-type Arc = (Point, Length, Angle)
+type Arc = (Point, Length, Angle, Angle)
+
+data Drawing = NotDrawing | DrawingLine Line | DrawingArc Arc
 
 data Gesture = NoTouches
 
              | Translate Point NTouch
 
-             | TwoFingers NTouch NTouch
-
+             | TwoFingers NTouch NTouch -- before we've figured out which one
              | AdjustLength Length NTouch NTouch
              | Rotate Angle NTouch NTouch
 
-             | OneFinger NTouch
+             | OneFinger NTouch -- right after you let go of adj-len or rot, the first finger shouldn't translate
 
-             | OneNTouchOneNTouch Point NTouch NTouch
-             | DrawLine NTouch NTouch Line
-             | DrawArc NTouch NTouch Arc
+             | OneFingerOnePencil NTouch NTouch -- before we've figured out which one
+             | DrawLine NTouch NTouch Point
+             | DrawArc NTouch NTouch Point Length
 
 -- updating state
 initialDrawState : DrawState
 initialDrawState = { lines = []
                    , arcs = []
-                   , toolCenter = (0, 0)
+                   , toolStart = (0, 0)
                    , toolAngle = 0
                    , toolLength = 300
-                   , gesture = NoTouches }
+                   , gesture = NoTouches
+                   , drawing = NotDrawing }
 
 norm : Point -> Float
 norm (x, y) = sqrt <| x^2 + y^2
 
 addv (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+subv (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 
 displacement : NTouch -> Point
 displacement t = (t.x - t.x0, t.y - t.y0)
@@ -55,21 +59,37 @@ displacement t = (t.x - t.x0, t.y - t.y0)
 angularDist : NTouch -> Angle
 angularDist t = atan2 (t.y - t.y0) (t.x - t.x0)
 
--- f1 is the fixed finger (the one that went down first)
--- f2 is the finger that's moving to do rotation or length adjustment
+toolEnd : Point -> Angle -> Length -> Point
+toolEnd (sx, sy) a l = (sx + l*(cos a), sy + l*(sin a))
+
+insideRuler : DrawState -> NTouch -> Bool
+insideRuler ds t = let end = toolEnd ds.toolStart ds.toolAngle ds.toolLength
+                   in norm (end `subv` (t.x, t.y)) < 100
+
+-- t1 is the fixed finger (the one that went down first)
+-- t2 is the finger that's moving to do rotation or length adjustment
 startTwoFingerGesture : NTouch -> NTouch -> DrawState -> Gesture
-startTwoFingerGesture f1 f2 ds =
-  -- do some tan stuff to find the angle of the movement from f2's p0 to f2's p
+startTwoFingerGesture t1 t2 ds =
+  -- do some tan stuff to find the angle of the movement from t2's p0 to t2's p
   -- then return AdjustLength or Rotate
-  let angle = angularDist f2 - ds.toolAngle
+  let angle = angularDist t2 - ds.toolAngle
   in if (angle < degrees 45 && angle > degrees -45) || (angle > degrees 135 || angle < degrees -135)
-       then AdjustLength ds.toolLength f1 f2 -- pulling left or right
-       else Rotate ds.toolAngle f1 f2
+       then AdjustLength ds.toolLength t1 t2 -- pulling left or right
+       else Rotate ds.toolAngle t1 t2
+
+-- t1 is the fixed finger (the one that went down first)
+-- t2 is the finger that's moving to draw a line or an arc
+startFingerPencilGesture : NTouch -> NTouch -> DrawState -> Gesture
+startFingerPencilGesture t1 t2 ds =
+  let angle = angularDist t2 - ds.toolAngle
+  in if (angle < degrees 45 && angle > degrees -45) || (angle > degrees 135 || angle < degrees -135)
+       then DrawLine t1 t2 ds.toolStart
+       else DrawArc t1 t2 ds.toolStart ds.toolLength
 
 gesture : [NTouch] -> DrawState -> Gesture
-gesture ts ds = Debug.log "gesture" <|
+gesture ts ds =
   case (ds.gesture, ts) of
-    (NoTouches, t::[]) -> Translate ds.toolCenter t
+    (NoTouches, t::[]) -> Translate ds.toolStart t
 
     (Translate p0 t, t'::[]) ->
       if t'.id == t.id
@@ -78,13 +98,18 @@ gesture ts ds = Debug.log "gesture" <|
 
     (Translate _ t1, t2'::t1'::[]) ->
       if t1'.id == t1.id
-        then TwoFingers t1' t2'
+        then if insideRuler ds t2'
+               then OneFingerOnePencil t1' t2'
+               else TwoFingers t1' t2'
         else NoTouches
     (OneFinger t1, t2'::t1'::[]) ->
       if t1'.id == t1.id
-        then TwoFingers t1' t2'
+        then if insideRuler ds t2'
+               then OneFingerOnePencil t1' t2'
+               else TwoFingers t1' t2'
         else NoTouches
 
+    -- two finger rotate/adj length stuff
     (TwoFingers t1 t2, t2'::t1'::[]) ->
       -- dispatch only if the distance t2 has moved is big enough
       if t1'.id == t1.id && t2'.id == t2.id
@@ -116,6 +141,33 @@ gesture ts ds = Debug.log "gesture" <|
         then OneFinger t'
         else NoTouches
 
+    -- pencil stuff
+    (OneFingerOnePencil t1 t2, t2'::t1'::[]) ->
+      if t1'.id == t1.id && t2'.id == t2.id
+        then if norm (displacement t2') > 100
+               then startFingerPencilGesture t1' t2' ds
+               else OneFingerOnePencil t1 t2
+        else NoTouches
+
+    (DrawLine t1 t2 l, t2'::t1'::[]) ->
+      if t1'.id == t1.id && t2'.id == t2.id
+        then DrawLine t1' t2' l
+        else NoTouches
+
+    (DrawArc t1 t2 c r, t2'::t1'::[]) ->
+      if t1'.id == t1.id && t2'.id == t2.id
+        then DrawArc t1' t2' c r
+        else NoTouches
+
+    (DrawLine t1 t2 _, t'::[]) ->
+      if t'.id == t1.id || t'.id == t2.id 
+        then OneFinger t'
+        else NoTouches
+    (DrawArc t1 t2 _ _, t'::[]) -> 
+      if t'.id == t1.id || t'.id == t2.id 
+        then OneFinger t'
+        else NoTouches
+
     (_, _) -> NoTouches
 
 update : [NTouch] -> DrawState -> DrawState
@@ -123,12 +175,19 @@ update ts ds =
   let gest = gesture ts ds
       ds' = { ds | gesture <- gest }
   in case gest of
-       Translate p0 t -> { ds' | toolCenter <- p0 `addv` displacement t }
+       Translate p0 t -> { ds' | toolStart <- p0 `addv` displacement t }
        AdjustLength l0 _ t2 -> { ds' | toolLength <- l0 + (t2.x - t2.x0) * cos ds'.toolAngle + (t2.y - t2.y0) * sin ds'.toolAngle }
        Rotate a0 t1 t2 ->
-         let (tx, ty) = ds'.toolCenter
+         let (tx, ty) = ds'.toolStart
          in { ds' | toolAngle <- atan2 (t2.y - ty) (t2.x - tx) }
-       otherwise -> ds'
+
+       DrawLine t1 t2 (sx, sy) ->
+         let (ex, ey) = displacement t2
+         in { ds' | drawing <- DrawingLine ((sx, sy), (sx + ex*(cos ds.toolAngle), sy + ey*(sin ds.toolAngle))) }
+       DrawArc t1 t2 c r ->
+         { ds' | drawing <- DrawingArc (c, ds'.toolAngle, angularDist t2, r) }
+
+       otherwise -> { ds' | drawing <- NotDrawing } -- TODO get rid of drawing earlier
 
 -- display
 unitWidth = 20
@@ -168,9 +227,13 @@ displayTool c angle len = group [ toolTop len |> moveY 11
                         |> rotate angle
 
 display : Int -> Int -> DrawState -> Element
-display ww wh { lines, arcs, toolCenter, toolAngle, toolLength } =
-        let linesForms = map (traced defaultLine . uncurry segment) <| lines
-        in collage ww wh <| linesForms ++ [displayTool toolCenter toolAngle toolLength]
+display ww wh { lines, arcs, toolStart, toolAngle, toolLength, drawing } =
+        let linesForms = map (traced defaultLine . uncurry segment)
+                         <| case drawing of
+                              NotDrawing -> lines
+                              DrawingLine l -> l :: lines
+                              DrawingArc _ -> lines
+        in collage ww wh <| linesForms ++ [displayTool toolStart toolAngle toolLength]
 
 -- problem display
 problem : Form
