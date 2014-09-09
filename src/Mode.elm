28195996a2518eca5249f62941e29b569177a5a0
Omar Rizwan <omar.rizwan@gmail.com>
2014-09-08 20:07:28 -0700

    fix mode touch in chrome with a hack

--- src/Mode.elm
+++ src/Mode.elm
@@ -3,17 +3,34 @@ module Mode where
 import Graphics.Input as I
 import Types (Mode, Ruler, Draw, Erase)
 
+import Window
+import Touch
+
 modes : [Mode]
 modes = [Ruler, Draw, Erase]
 
 modeI : I.Input Mode
 modeI = I.input Ruler
 
-mode : Signal Mode
-mode = modeI.signal
-
 boxWidth = 150
 boxHeight = 300
+
+dimTaps : Signal ((Int, Int), { x : Int, y : Int })
+dimTaps = lift2 (,) Window.dimensions Touch.taps
+
+inBox : ((Int, Int), { x : Int, y : Int }) -> Bool
+inBox ((ww, wh), {x, y}) = x > ww - boxWidth && y < boxHeight
+
+tapToMode : { x : Int, y : Int } -> Mode
+tapToMode {x, y} =
+  let idx = y `div` (boxHeight `div` length modes)
+  in head <| drop idx modes
+
+tappedMode : Signal Mode
+tappedMode = tapToMode . snd <~ keepIf inBox ((0,0),{x=0,y=0}) dimTaps
+
+mode : Signal Mode
+mode = merge tappedMode modeI.signal
 
 modeButton : Mode -> Mode -> Element
 modeButton selectedMode m =
